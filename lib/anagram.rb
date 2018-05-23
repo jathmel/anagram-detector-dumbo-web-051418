a04773fbbdbcba6513f02d3091eace71e667546f
github username <github email address>
2018-05-23 16:35:55 -0400

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+  def match(list)
+    result = []
+    list.each do |word|
+      if word.downcase.chars.sort == @word.downcase.chars.sort
+        result  << word
+      end
+    end
+    result
+  end
+end
+
 # Your code goes here!
